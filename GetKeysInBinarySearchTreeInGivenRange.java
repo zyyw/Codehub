e4d1b70805e19f0aaf7c63742fe8fdfdef576f23
Shengwen-Yu <yushengwen18@gmail.com>
2017-08-12 20:23:41 -0400

    LC-76; added solution for "get keys in binary search tree in given range"

---
+++ GetKeysInBinarySearchTreeInGivenRange.java
@@ -0,0 +1,30 @@
+/**
+ * Problem statement:
+ * Get the list of keys in a given binary search tree in a given range [min, max] in ascending order, both min and max are inclusive.
+ *
+ * In ascending order ==> 中序遍历
+ *
+ */
+
+public class GetKeysInBinarySearchTreeInGivenRange {
+	public List<Integer> getRange(TreeNode root, int min, int max) {
+		List<Integer> ret = new ArrayList<>();
+		getRangeHelper(root, min, max, ret);
+		return ret;
+	}
+
+	private void getRangeHelper(TreeNode root, int min, int max, List<Integer> ret) {
+		if (root == null) {
+			return;
+		}
+		if (min < root.key) {
+			getRangeHelper(root.left, min, max, ret);
+		}
+		if (min <= root.key && root.key <= max) {
+			ret.add(root.key);
+		}
+		if (root.key < max) {
+			getRangeHelper(root.right, min, max, ret);
+		}
+	}
+}
