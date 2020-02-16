5ae0c66af1d3b035246753438541e9888d1483e6
Shengwen-Yu <yushengwen18@gmail.com>
2020-02-16 10:57:24 +0800

    fix: update max water trapped

--- MaxWaterTrapped2.java
+++ MaxWaterTrapped2.java
@@ -11,6 +11,12 @@
  * 如果不放四个角上的点，点 1 回被算到 water 里去 (3 - 1)
  * expect: 1
  * actual: 12 (如果不放四个角上的点)
+ *
+ * 思路：
+ * 用一个minHeap 装边界柱高，从中取最矮的柱高，往其上下左右搜索。
+ * 如果遇到较高的柱高，则将其放入 minHeap 作为新边界；
+ * 反之，如果遇到较低的柱高，则累加该柱子可以盛水的体积 (从minHeap取出的柱高-该较低的柱高），
+ * 然后仍将取出的柱高作为新边界柱高放回 minHeap 中。
  */
 
 public class MaxWaterTrapped2 {
@@ -18,14 +24,10 @@ public class MaxWaterTrapped2 {
 		int x;
 		int y;
 		int val;
-		Cell(int x, int y, int val) {
-			this.x = x;
-			this.y = y;
-			this.val = val;
-		}
-		@Override
-		public int compareTo(Cell cell) {
-			return this.val < cell.val ? -1 : 1; // minHeap
+		Cell(int i, int j, int val) {
+			x = i;
+			y = j;
+			val = val;
 		}
 	}
 
@@ -34,11 +36,16 @@ public class MaxWaterTrapped2 {
 		if (heights == null || heights.length <= 2 || heights[0] == null || heights[0].length <= 2) {
 			return 0;
 		}
-		Queue<Cell> pq = new PriorityQueue<>();
 		int m = heights.length;
 		int n = heights[0].length;
+
+		Queue<Cell> pq = new PriorityQueue<>(1, new Comparator<Cell>(){
+			public int compare(Cell c1, Cell c2) {
+				return c1.val < c2.val ? -1 : 1;
+			}
+		});
 		boolean[][] visited = new boolean[m][n];
-		for (int i = 1; i < m - 1; ++i) {
+		for (int i = 0; i < m; ++i) {
 			// first column
 			pq.offer(new Cell(i, 0, heights[i][0]));
 			visited[i][0] = true;
@@ -46,7 +53,7 @@ public class MaxWaterTrapped2 {
 			pq.offer(new Cell(i, n - 1, heights[i][n - 1]));
 			visited[i][n - 1] = true;
 		}
-		for (int j = 0; j < n; ++j) {
+		for (int j = 1; j < n - 1; ++j) {
 			// first row
 			pq.offer(new Cell(0, j, heights[0][j]));
 			visited[0][j] = true;
@@ -54,12 +61,12 @@ public class MaxWaterTrapped2 {
 			pq.offer(new Cell(m - 1, j, heights[m - 1][j]));
 			visited[m - 1][j] = true;
 		}
+
 		int water = 0;
-		Cell cur = null;
 		int[] dx = {1, -1, 0, 0};
 		int[] dy = {0, 0, 1, -1};
 		while (!pq.isEmpty()) {
-			cur = pq.poll();
+			Cell cur = pq.poll();
 			for (int i = 0; i < 4; ++i) {
 				int x2 = cur.x + dx[i];
 				int y2 = cur.y + dy[i];
